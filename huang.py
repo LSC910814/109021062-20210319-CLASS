227c5fa8c12b7f6529416593cfd909320579f9dd
LSC910814 <109021062@live.asia.edu.tw>
2021-03-27 11:03:35 +0800

    update files commit

--- huang.py
+++ huang.py
@@ -2,19 +2,18 @@ import requests as reqs
 from bs4 import BeautifulSoup
 
 req=reqs.get(
-    "https://www.intechopen.com/books/bioinformatics-in-the-era-of-post-genomics-and-big-data/a-novel-approach-to-mine-for-genetic-markers-via-comparing-class-frequency-distributions-of-maximal-")
-req.encoding="utf8"
+    "http://isrc.ccs.asia.edu.tw/www/myjournal/myjournal.htm")
+req.encoding="big5"
 if req.status_code == 200:
     #print(req.text)
     soup = BeautifulSoup(req.text, "lxml")
-    result1 = soup.find_all("li")
-    fp = open("out2.txt", "w", encoding="utf8")
+    result1 = soup.find_all("p")
+    fp = open("huang.txt", "w", encoding="utf8")
     for val in result1:
         text2 = val.text.replace('\n', '')
         text3 = text2.replace('  ','')
         print(text3)
         fp.write(text3+"\n")
     fp.close()
-
 else:
     print("no page")
--- wang.py
+++ wang.py
@@ -2,19 +2,18 @@ import requests as reqs
 from bs4 import BeautifulSoup
 
 req=reqs.get(
-    "https://www.intechopen.com/books/bioinformatics-in-the-era-of-post-genomics-and-big-data/a-novel-approach-to-mine-for-genetic-markers-via-comparing-class-frequency-distributions-of-maximal-")
-req.encoding="utf8"
+    "http://dns2.asia.edu.tw/~jdwang/PaperList.htm")
+req.encoding="big5"
 if req.status_code == 200:
     #print(req.text)
     soup = BeautifulSoup(req.text, "lxml")
-    result1 = soup.find_all("li")
-    fp = open("out2.txt", "w", encoding="utf8")
+    result1 = soup.find_all(["li","ul","ol","p","blockquote"])
+    fp = open("wang.txt", "w", encoding="utf8")
     for val in result1:
         text2 = val.text.replace('\n', '')
         text3 = text2.replace('  ','')
         print(text3)
         fp.write(text3+"\n")
     fp.close()
-
 else:
     print("no page")
